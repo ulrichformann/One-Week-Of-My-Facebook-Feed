155b64352802a34f099b4cfe19793ae978bd905d
Ulrich Formann <m@ul.fo>
2020-08-12 22:09:03 +0200

    updated code for new facebook design

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # One-Week-Of-My-Facebook-Feed-On-Toilet-Paper
  
+## append images
+convert 'facebook/*.png' -gravity center -append out2.png
--- src/index.js
+++ src/index.js
@@ -3,54 +3,63 @@ require('dotenv').config()
 const puppeteer = require('puppeteer')
 const fs = require('fs-extra')
 
-const timeStampSelector = '#contentArea div:not(._5pcm) > div > span:not(.x_p2l_v7v1o) > span > a > abbr > span.timestampContent[id]'
+init().then(async ({ page, browser }) => {
+  let endHeight = 0
 
-  ; (async () => {
-    const { page, browser } = await init()
+  while (endHeight === 0) {
+    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
+    await page.waitFor(1000)
 
-    /*let endHeight = 0
-  
-    while (endHeight === 0) {
-      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
-      await page.waitFor(1000)
-  
-      endHeight = await page.$$eval(timeStampSelector, ts => {
-        const index = ts.findIndex(t => t.textContent.indexOf('Std') != -1)
-  
-        if (index === -1)
-          return 0
-        else {
-          const last = ts[index - 1].closest('.userContentWrapper').getBoundingClientRect()
-          return last.top + window.pageYOffset + last.height
-        }
+    endHeight = await page.$$eval('[role="article"]', els => {
+      const index = els.findIndex(e => {
+        const s = e.textContent.split('·')
+
+        if (s.length <= 1)
+          return false
+
+        console.log(s[1].replace(/-/g, '').trim())
+
+        return s[1].replace(/-/g, '').trim().indexOf('12 Std.') != -1
       })
+
+      console.log(index)
+
+      if (index === -1)
+        return 0
+      else {
+        const last = els[index - 1].getBoundingClientRect()
+        return last.top + window.pageYOffset + last.height + 4
+      }
+    })
   }
 
   console.log(endHeight)
 
-for (var i = 0; i < Math.ceil(endHeight / 1000); i++) {
-  await page.evaluate(`window.scrollTo(0, ${i * 1000})`)
+  await fs.emptyDir('output/facebook')
 
-  console.log('screenshot ' + i)
+  for (var i = 0; i < Math.ceil(endHeight / 1000); i++) {
+    await page.evaluate(`window.scrollTo(0, ${i * 1000})`)
 
-  console.log(endHeight % 1000, endHeight)
+    console.log('screenshot ' + i)
 
-  await page.screenshot({
-    fullPage: false,
-    path: `output/facebook/${pad(i, 4)}.png`,
-    type: 'png',
-    clip: {
-      x: 275,
-      y: i * 1000,
-      width: 522,
-      height: (i === Math.floor(endHeight / 1000)) ? (endHeight % 1000) : 1000
-    },
-    omitBackground: true
-  })
-}
+    console.log(endHeight % 1000, endHeight)
 
-await browser.close()*/
-  })()
+    await page.screenshot({
+      fullPage: false,
+      path: `output/facebook/${pad(i, 4)}.png`,
+      type: 'png',
+      clip: {
+        x: 310,
+        y: i * 1000,
+        width: 600,
+        height: (i === Math.floor(endHeight / 1000)) ? (endHeight % 1000) : 1000
+      },
+      omitBackground: true
+    })
+  }
+
+  await browser.close()
+})
 
 function pad(num, size) {
   var s = num + "";
@@ -62,16 +71,20 @@ async function init() {
   const browser = await puppeteer.launch({ headless: false })
 
   const page = await browser.newPage()
-  await page.setViewport({ width: 1382, height: 1000 })
+  await page.setViewport({ width: 1220, height: 1000 })
 
   const cookies = await fs.readJson('cookies/facebook.json')
   await page.setCookie(...cookies)
 
   await page.goto('https://www.facebook.com/?sk=h_chr', { waitUntil: 'networkidle2' })
 
-  await page.evaluate(`document.querySelector('#blueBarDOMInspector').remove()`)
   await page.evaluate(`document.querySelector('body').style.background = 'initial'`)
-  await page.evaluate(`document.querySelector('#contentCol').style.background = 'initial'`)
+  await page.evaluate(`document.querySelector('[data-pagelet="page"]').parentElement.style.top = 'initial'`)
+  await page.evaluate(`document.querySelector('[role="banner"]').remove()`)
+  await page.evaluate(`document.querySelector('[aria-label="Alle Stories ansehen"]').remove()`)
+  // await page.evaluate(`document.querySelector('[data-pagelet="Stories"]').parentElement.remove()`)
+  // await page.evaluate(`document.querySelector('[role="main"]').firstChild.firstChild.style['margin-top'] = 'initial'`)
+  // await page.evaluate(`document.querySelector('[role="main"]').firstChild.firstChild.children[1].remove()`)
 
   return { page, browser }
 }
